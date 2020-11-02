8fd59f69203f93839e160a24e35af15ba1a3debc
Denis Korotin <dkorotin@list.ru>
2020-11-02 15:27:05 +0700

    14 lesson циклы

---
+++ js/JS_Section_1/script14.js
@@ -0,0 +1,29 @@
+/*14 урок. Циклы.*/
+"use strict"; //директива для объявления скриптовому файлу, что мы работаем в современном режиме
+
+let num = 50;
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+let nums = 50;
+do {
+    console.log(nums);
+    nums++;
+}
+while (nums < 55);
+
+numsf = 50;
+for (let i = 1; i < 8; i++) {
+    console.log(numsf);
+    numsf++;
+}
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) { //прервать выполнение цикла по условию
+        //break;
+        continue;
+    }
+    console.log(i);
+}
