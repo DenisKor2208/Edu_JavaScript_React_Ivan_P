7a9657455c42b8ba28aea2b3b26bd16bde438c15
Denis Korotin <dkorotin@list.ru>
2020-11-02 10:28:44 +0700

    12 lesson part 1

--- js/script12.js
+++ js/script12.js
@@ -1,3 +1,21 @@
-/*12 урок. Интерполяция.*/
+/*12 урок. Практика ч.1 Начинаем создавать приложение.*/
 "use strict"; //директива для объявления скриптовому файлу, что мы работаем в современном режиме
 
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", ""),
+    personaMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actor: {},
+        genres: [],
+        privat: false
+    };
+
+let questionNameOneFilm = prompt("Один из последних просмотренных фильмов?", ""),
+    questionRatingOneFilm = prompt("На сколько оцените его?", ""),
+    questionNameTwoFilm = prompt("Один из последних просмотренных фильмов?", ""),
+    questionRatingTwoFilm = prompt("На сколько оцените его?", "");
+
+personaMovieDB.movies[questionNameOneFilm] = questionRatingOneFilm;
+personaMovieDB.movies[questionNameTwoFilm] = questionRatingTwoFilm;
+
+console.log(personaMovieDB);
